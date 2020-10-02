80be2713391294b06abd7a920c95f06d51e94ad4
jpb06 <jpb.06@outlook.com>
2020-10-02 20:59:14 +0200

    Wrapping up

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { GapiState } from "./hooks/use.gapi.loading.hook";
+import { useGapiLogin } from "./hooks/use.gapi.login.hook";
+import { GapiConfig } from "./logic/gapi.config.logic";
+
+export { useGapiLogin, GapiConfig, GapiState };
