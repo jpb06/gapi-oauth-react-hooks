d2aacf99213e82f5ccc89a182959938df35a0a84
jpb06 <jpb.06@outlook.com>
2020-10-04 18:04:02 +0200

    Amending mocks & mock data

--- src/tests-related/mocks/gapi/auth2.google.auth.mock.ts
+++ src/tests-related/mocks/gapi/auth2.google.auth.mock.ts
@@ -3,11 +3,14 @@ import { mockGapiCurrentUser, MockUser } from "./auth2.current.user.mock";
 export const mockGoogleAuth = (
   isSignedIn: boolean,
   user?: MockUser,
+  authResponse?: gapi.auth2.AuthResponse,
   thenFn: (res: any, err: any) => void = jest.fn(),
   signOutMockFn = jest.fn(),
   disconnectMockFn = jest.fn()
 ): gapi.auth2.GoogleAuth => ({
-  signIn: jest.fn().mockReturnValue(mockGapiCurrentUser(user)?.get()),
+  signIn: jest
+    .fn()
+    .mockReturnValue(mockGapiCurrentUser(user, authResponse)?.get()),
   signOut: signOutMockFn,
   disconnect: disconnectMockFn,
   grantOfflineAccess: jest.fn(),
@@ -17,5 +20,5 @@ export const mockGoogleAuth = (
     get: jest.fn().mockReturnValue(isSignedIn),
     listen: jest.fn(),
   },
-  currentUser: mockGapiCurrentUser(user),
+  currentUser: mockGapiCurrentUser(user, authResponse),
 });
