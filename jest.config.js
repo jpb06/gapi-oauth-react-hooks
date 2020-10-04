359a61b00eeb0b52e6b0a5ba8502d5b5698e9ec4
jpb06 <jpb.06@outlook.com>
2020-10-04 14:17:27 +0200

    Jest config

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+   preset: "ts-jest",
+   setupFilesAfterEnv: ["./setupTests.ts"],
+   coverageReporters: [
+      "json-summary",
+      "text",
+      "lcov"
+   ],
+   collectCoverage: true,
+   collectCoverageFrom: [
+      "src/**/*.{ts,tsx}",
+      "!<rootDir>/node_modules/",
+      "!<rootDir>/dist/",
+      "!<rootDir>/src/index.ts",
+      "!<rootDir>/src/indirection/**",
+      "!<rootDir>/src/tests-related/**"
+   ]
+};
