e74d6eeffdedf46b2b3714ea1350e0825dff4fc3
jpb06 <jpb.06@outlook.com>
2020-10-08 18:45:07 +0200

    Exporting the converter and its associated interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { GoogleAuthHookProps, useGoogleAuth } from "./hooks/use.google.auth.hook";
+import {
+  GoogleAuthHookProps,
+  useGoogleAuth,
+} from "./hooks/use.google.auth.hook";
 import { GapiConfig } from "./logic/gapi.config.logic";
 import { GapiState } from "./types/gapiState";
+import { UserProfile } from "./types/user.profile";
 
 export { useGoogleAuth, GapiConfig };
 export type { GoogleAuthHookProps };
 export type { GapiState };
+export type { UserProfile };
