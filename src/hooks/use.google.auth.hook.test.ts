6072a3cf3a2a627854001cdbbb0eafb530a8e07b
jpb06 <jp.bois.06@outlook.fr>
2022-08-03 17:05:14 +0200

    chore: misc deps bump related changes

--- src/hooks/use.google.auth.hook.test.ts
+++ src/hooks/use.google.auth.hook.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import { mocked } from 'jest-mock';
 
 import { gapiGetAuth2Instance } from '../indirection/gapi.lib.indirection';
