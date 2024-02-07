9b6d61f619c96fad19592ccd4da07a579390bdbf
jpb06 <jp.bois.06@outlook.fr>
2024-02-07 19:01:28 +0100

    fix: jest badges generation

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     ],
     globals: false,
     coverage: {
-      reporter: ['text', 'json', 'html', 'lcov'],
+      reporter: ['json-summary'],
       all: true,
       include: ['src/**/*.ts', 'src/**/*.tsx'],
       exclude: ['src/tests/**/*', 'src/**/*.type.ts', 'src/**/*/index.ts'],
